f7027f1efcc6ae1f96fa3bffd179ca4017e2ff94
David Veríssimo Leite <103391404+davidvleite@users.noreply.github.com>
2022-08-13 00:57:11 -0300

    Revert "SecondMigration"
    This reverts commit bc2a81056787abd3f184dd0a0241b6e4d5116573.

--- Data/SalesWebMvcContext.cs
+++ Data/SalesWebMvcContext.cs
@@ -12,8 +12,7 @@ namespace SalesWebMvc.Models
             : base(options)
         {
         }
-        public DbSet<Department> Department { get; set; }
-        public DbSet<Seller> Seller { get; set; }
-        public DbSet<SalesRecord> SalesRecord { get; set; }
+
+        public DbSet<SalesWebMvc.Models.Department> Department { get; set; }
     }
 }
